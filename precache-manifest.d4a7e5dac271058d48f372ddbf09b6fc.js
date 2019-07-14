ff273b02578f8f880e48ea74a6173e612dd02622
Stanislav Kaisin <8050361@gmail.com>
2019-07-14 11:55:52 +0300

    Updates

---
+++ precache-manifest.d4a7e5dac271058d48f372ddbf09b6fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5cd39b84ec3f86421dcdd694a9a9b0",
+    "url": "/goit-react-hw-02/index.html"
+  },
+  {
+    "revision": "3fd1026fe1a4f1db3647",
+    "url": "/goit-react-hw-02/static/css/main.2536e88c.chunk.css"
+  },
+  {
+    "revision": "5726e0c79b7851ddd956",
+    "url": "/goit-react-hw-02/static/js/2.8f69a87c.chunk.js"
+  },
+  {
+    "revision": "3fd1026fe1a4f1db3647",
+    "url": "/goit-react-hw-02/static/js/main.371ceaf8.chunk.js"
+  },
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  }
+]);
